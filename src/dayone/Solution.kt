08e7d60b3411adfc1c834838e12921de9a096a6d
YayaADev <yayaahmed1@gmail.com>
2025-12-03 18:31:22 -0800

    Day one pt2 solution

--- src/dayone/Solution.kt
+++ src/dayone/Solution.kt
@@ -2,26 +2,50 @@ package dayone
 
 import java.io.File
 
+var sumOfZeros = 0
+
 fun main() {
-    var sumOfZeros = 0
     var previousDial = 50
     val inputFile = File("src/dayone/input.txt")
     val leftOrRightDial = inputFile.readLines().map { line ->
         line[0] to line.substring(1).toInt()
     }
 
-    leftOrRightDial.forEach { (first, second) ->
-        if (first == 'L') {
-            previousDial = (previousDial - second) % 100
-            println("Moving Left new dial is $previousDial")
-        } else if (first == 'R') {
-            previousDial = (previousDial + second) % 100
-            println("Moving Right new dial is $previousDial")
+    leftOrRightDial.forEach { (rotation, amount) ->
+        if (rotation == 'L') {
+            previousDial = calculateLeftDial(previousDial, amount)
+        } else if (rotation == 'R') {
+            previousDial = calculateRightDial(previousDial, amount)
         }
-        if (previousDial == 0) {
-            sumOfZeros++
-        }
+
     }
     print(sumOfZeros)
 }
 
+fun calculateLeftDial(startingPosition: Int, leftRotation: Int): Int {
+    val laps = leftRotation / 100
+    val leftover = leftRotation.mod(100)
+    val newDial = startingPosition - leftover
+
+    sumOfZeros += laps
+
+    if (startingPosition > 0 && newDial <= 0) {
+        sumOfZeros++
+    }
+
+    return newDial.mod(100)
+}
+
+fun calculateRightDial(startingPosition: Int, rightRotation: Int): Int {
+    val laps = rightRotation / 100
+    val leftover = rightRotation.mod(100)
+    val newDial = startingPosition + leftover
+
+    sumOfZeros += laps
+
+    if (newDial >= 100) {
+        sumOfZeros++
+    }
+
+    return newDial.mod(100)
+}
