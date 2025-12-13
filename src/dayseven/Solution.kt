20de9d1258428f22be9745c88bd70dba4e84f496
YayaADev <yayaahmed1@gmail.com>
2025-12-12 23:30:22 -0800

    day7 pt2

--- src/dayseven/Solution.kt
+++ src/dayseven/Solution.kt
@@ -2,31 +2,35 @@ package dayseven
 
 import java.io.File
 var globalTracker = 0L
+var cache = mutableMapOf<Pair<Int, Int>, Long>()
 fun main() {
     var input = File("src/dayseven/day7input.txt").readLines().map { line -> line.toCharArray()}
     val startingS = input.first().indexOf('S')
 
-    placePipes(input, Pair(1, startingS))
-    println(globalTracker)
+    val totla = placePipes(input, Pair(1, startingS))
+    println(totla)
 
 
 }
 
-fun placePipes(input: List<CharArray>, startingCoord: Pair<Int, Int>) {
-    println(startingCoord)
-    if (startingCoord.first >= input.size) return
+fun placePipes(input: List<CharArray>, startingCoord: Pair<Int, Int>) : Long {
+    if (cache.containsKey(startingCoord)) return cache[startingCoord]!!
+
+    if (startingCoord.first >= input.size) return 1
+
 
     if (input[startingCoord.first][startingCoord.second] == '.') {
-        input[startingCoord.first][startingCoord.second] = '|'
-        placePipes(input, Pair(startingCoord.first + 1, startingCoord.second))
+        return placePipes(input, Pair(startingCoord.first + 1, startingCoord.second))
     }
     if (input[startingCoord.first][startingCoord.second] == '^') {
-        globalTracker++
         val leftOfUp = Pair(startingCoord.first, startingCoord.second-1)
         val rightOfUp = Pair(startingCoord.first, startingCoord.second+1)
-        placePipes(input, leftOfUp)
-        placePipes(input, rightOfUp)
+        val leftCount = placePipes(input, leftOfUp)
+        val rightCount = placePipes(input, rightOfUp)
+        cache[startingCoord] =  leftCount + rightCount
+        return leftCount + rightCount
     }
+    return 0
 }
 
 
